77d014c7b5d69c00fad75fb25c06958a586b33db
BrenerViotto <brenerviotto@gmail.com>
2024-09-12 19:18:06 -0300

    service

---
+++ migrations/models/0_20240912191125_init.py
@@ -0,0 +1,48 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "indicators" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL UNIQUE,
+    "is_active" BOOL NOT NULL  DEFAULT True,
+    "differential" DECIMAL(10,2) NOT NULL,
+    "symbol" VARCHAR(50) NOT NULL,
+    "tenant" INT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "symbols" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "code" VARCHAR(50) NOT NULL UNIQUE,
+    "is_active" BOOL NOT NULL  DEFAULT True,
+    "tenant" INT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "tenants" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL UNIQUE,
+    "owner" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "is_active" BOOL NOT NULL  DEFAULT True
+);
+CREATE TABLE IF NOT EXISTS "users" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "username" VARCHAR(150) NOT NULL UNIQUE,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "full_name" VARCHAR(255) NOT NULL,
+    "hashed_password" VARCHAR(255) NOT NULL,
+    "is_active" BOOL NOT NULL  DEFAULT True,
+    "is_superuser" BOOL NOT NULL  DEFAULT False,
+    "tenant" INT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
